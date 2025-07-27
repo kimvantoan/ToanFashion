406189f3baa9182be91d3858abc29bcba67f5c3f
kimtoandev <kimvantoan2k3@gmail.com>
2025-07-27 09:50:33 +0700

    env URL

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import cookieParser from 'cookie-parser';
-import router from './routes/index.route.js';
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import router from "./routes/index.route.js";
+import cors from "cors";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,22 +11,24 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
+const allowedOrigins = [process.env.CLIENT_URL, process.env.ADMIN_URL];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 
 // Routes
-app.use('/api/v1', router);
+app.use("/api/v1", router);
 
 connectDB();
 // Start server
