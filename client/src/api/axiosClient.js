4edc3ab9f0c3c5c8fb950cebcd4375af978b415b
kimtoandev <kimvantoan2k3@gmail.com>
2025-07-27 10:23:05 +0700

    refactor: change baseURL

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, 
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
+  withCredentials: true,
 });
 
 axiosClient.interceptors.request.use(
